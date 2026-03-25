44537aed9fd282ac30c84171b6789d9c8f0d1aba
ankit-thesys <i-ankit@thesys.dev>
2026-03-25 11:53:46 +0530

    added missing export

--- packages/react-ui/src/components/Charts/index.ts
+++ packages/react-ui/src/components/Charts/index.ts
@@ -1,13 +1,32 @@
-export { AreaChart as ScrollableAreaChart } from "./AreaChart";
+export {
+  AreaChart as ScrollableAreaChart,
+  type AreaChartProps as ScrollableAreaChartProps,
+} from "./AreaChart";
+export {
+  type AreaChartData as ScrollableAreaChartData,
+  type AreaChartVariant as ScrollableAreaChartVariant,
+} from "./AreaChart/types";
 export * from "./AreaChartCondensed";
-export { AreaChartCondensed as AreaChart } from "./AreaChartCondensed";
-export { BarChart as ScrollableBarChart } from "./BarChart";
+export {
+  BarChart as ScrollableBarChart,
+  type BarChartProps as ScrollableBarChartProps,
+} from "./BarChart";
+export {
+  type BarChartData as ScrollableBarChartData,
+  type BarChartVariant as ScrollableBarChartVariant,
+} from "./BarChart/types";
 export * from "./BarChartCondensed";
-export { BarChartCondensed as BarChart } from "./BarChartCondensed";
 export * from "./HorizontalBarChart";
-export { LineChart as ScrollableLineChart } from "./LineChart";
+export {
+  LineChart as ScrollableLineChart,
+  type LineChartProps as ScrollableLineChartProps,
+} from "./LineChart";
+export {
+  type LineChartData as ScrollableLineChartData,
+  type LineChartVariant as ScrollableLineChartVariant,
+} from "./LineChart/types";
 export * from "./LineChartCondensed";
-export { LineChartCondensed as LineChart } from "./LineChartCondensed";
+
 export * from "./MiniAreaChart";
 export * from "./MiniBarChart";
 export * from "./MiniLineChart";
